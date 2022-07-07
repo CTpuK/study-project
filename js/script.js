6dc510b194be868b0b5c76327120959feabec35e
Evgheni Strikunov <strik.jenya@gmail.com>
2022-07-07 23:42:16 +0300

    day 07.07 of learning

--- js/script.js
+++ js/script.js
@@ -2,25 +2,50 @@
     'use strict';
 }());
 
-const str = "teSt";
-const arr = [1, 2, 3];
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        background: "red"
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
+};
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+options.makeTest();
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf("q"));
+const {border, background} = options.colors;
+console.log(border);
+console.log(background);
 
-const logg = "Hello World!";
 
-console.log(logg.slice(6));
+// console.log(Object.keys(options).length);
 
-console.log(logg.substring(6, 11));
 
-const num = 12.2;
-console.log(Math.round(num));
+// console.log(options["colors"]["border"]);
 
-const test = "12.2px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+
+// for (let key in options) {
+//     if (typeof (options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство  ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство  ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+
+
